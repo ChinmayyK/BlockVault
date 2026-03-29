a5a1b923d63280c77736b243eb6e0dbbfe8276d1
Chinmay Kudalkar <chinmaykudalkarrr@gmail.com>
2026-03-29 12:08:00 +0530

    Update card.tsx

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -50,4 +50,3 @@ const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
 CardFooter.displayName = "CardFooter";
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
-
